de2f8eea1a70a8adb45993ace73614a671fa1bc5
evervc-dev <ever.vasquezc27@gmail.com>
2026-04-30 08:46:10 -0600

    :wrench: chore(Properties & AppConfiguration): Creating and configuring the properties for jwt

--- src/main/java/com/evervc/dev/inventorymanagement/config/AppConfiguration.java
+++ src/main/java/com/evervc/dev/inventorymanagement/config/AppConfiguration.java
@@ -1,9 +1,11 @@
 package com.evervc.dev.inventorymanagement.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
+@PropertySource("classpath:jwt.properties")
 public class AppConfiguration {
 }
