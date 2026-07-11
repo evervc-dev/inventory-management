107a8875c19f58999699834dd7bd800aa9993913
Ever-VC <ever.vasquezc27@gmail.com>
2026-07-11 09:23:47 -0600

    :test_tube: test(mapper): Reorganize RoleMapperTest by moving and enhancing tests for DTO to Entity mapping and list mapping

--- src/test/java/com/evervc/dev/inventorymanagement/mapper/RoleMapperTest.java
+++ src/test/java/com/evervc/dev/inventorymanagement/mapper/RoleMapperTest.java
@@ -40,36 +40,6 @@ public class RoleMapperTest {
     }
 
     @Test
-    @DisplayName("Mapping role from DTO to Entity")
-    void shouldMapDtoToEntity() {
-        // Arrange
-        Role expected = new Role(null, "ADMIN");
-
-        // Act
-        Role role = roleMapper.toCreateEntity(new RoleRequestDto("ADMIN"));
-
-        // Assert
-        Assertions.assertEquals(expected, role);
-    }
-
-    @Test
-    @DisplayName("Mapping a list of roles to roles response DTO")
-    void shouldMapRolesToListOfRolesDto() {
-        // Arrange
-        List<Role> roles = Arrays.asList(new Role(1L, "ADMIN"), new Role(2L, "USER"));
-        List<RoleResponseDto> expectedDtos = Arrays.asList(
-                new RoleResponseDto(1L, "ADMIN"),
-                new RoleResponseDto(2L, "USER")
-        );
-
-        // Act
-        List<RoleResponseDto> responseDtos = roleMapper.toRolesDto(roles);
-
-        // Assert
-        Assertions.assertEquals(expectedDtos, responseDtos);
-    }
-
-    @Test
     @DisplayName("Try to mapping a null role from Entity to DTO")
     void shouldReturnNullWhenMappingNullgRole() {
         // Arrange Act & Assert
@@ -102,5 +72,35 @@ public class RoleMapperTest {
 
         // Assert
         Assertions.assertEquals(expected, responseDto);
+    }
+
+    @Test
+    @DisplayName("Mapping role from DTO to Entity")
+    void shouldMapDtoToEntity() {
+        // Arrange
+        Role expected = new Role(null, "ADMIN");
+
+        // Act
+        Role role = roleMapper.toCreateEntity(new RoleRequestDto("ADMIN"));
+
+        // Assert
+        Assertions.assertEquals(expected, role);
+    }
+
+    @Test
+    @DisplayName("Mapping a list of roles to roles response DTO")
+    void shouldMapRolesToListOfRolesDto() {
+        // Arrange
+        List<Role> roles = Arrays.asList(new Role(1L, "ADMIN"), new Role(2L, "USER"));
+        List<RoleResponseDto> expectedDtos = Arrays.asList(
+                new RoleResponseDto(1L, "ADMIN"),
+                new RoleResponseDto(2L, "USER")
+        );
+
+        // Act
+        List<RoleResponseDto> responseDtos = roleMapper.toRolesDto(roles);
+
+        // Assert
+        Assertions.assertEquals(expectedDtos, responseDtos);
     }
 }
